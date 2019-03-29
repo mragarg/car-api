c9ef7397d4db59e76fcce662374f2839b67e4c63
Ashish Garg <mragarg@gmail.com>
2019-03-29 15:14:43 -0400

    Adds first test case

--- test/test.js
+++ test/test.js
@@ -5,4 +5,11 @@ chai.use(chaiAsPromised).should();
 
 const Car = require('../models/car');
 
-describe('Car Model', () => {});
+describe('Car Model', () => {
+    
+    // Return an array of all cars in the database
+    it('should be able to return an array of cars', () => {
+
+    });
+    
+});
