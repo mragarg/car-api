a1897cfbe8d52d611a0ea94bc802ee7f94344889
Ashish Garg <mragarg@gmail.com>
2019-03-29 14:25:39 -0400

    Adds seed test data

---
+++ seed.sql
@@ -0,0 +1,12 @@
+insert into cars
+    (year, make, model, class)
+values
+    (2014, 'BMW', '550i', 'Sedan'),
+    (1999, 'testMake', 'testModel', 'testClass'),
+    (2016, 'Porsche', 'Cayenne GTS', 'SUV'),
+    (2010, 'testMake2', 'testModel2', 'testClass'),
+    (2016, 'BMW', '328i GT', 'Hatchback'),
+    (2007, 'Lexus', 'ES 350', 'Sedan'),
+    (2007, 'Lexus', 'RX 350', 'SUV')
+;
+
