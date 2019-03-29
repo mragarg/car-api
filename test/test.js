9509427d1b0ff268e7d47478232772eb3b624644
Ashish Garg <mragarg@gmail.com>
2019-03-29 15:18:56 -0400

    Adds a test case getAll()

--- test/test.js
+++ test/test.js
@@ -1,4 +1,4 @@
-const chair = require('chai');
+const chai = require('chai');
 const expect = chai.expect;
 const chaiAsPromised = require('chai-as-promised');
 chai.use(chaiAsPromised).should();
@@ -9,7 +9,12 @@ describe('Car Model', () => {
     
     // Return an array of all cars in the database
     it('should be able to return an array of cars', () => {
-
+        // call function to return an array of cars
+        const allCars = Car.getAll();
+        // expect instance to be an array
+        expect(allCars).to.be.an.instanceOf(Array);
+        // make sure the length is 7 (seed data)
+        expect(allCars).to.have.lengthOf(7)
     });
-    
+
 });
