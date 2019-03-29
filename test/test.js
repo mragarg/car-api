a2b830d3c3a78709dc7aedd96b6c9d6229c55df5
Ashish Garg <mragarg@gmail.com>
2019-03-29 15:13:28 -0400

    Adds Car model TDD

--- test/test.js
+++ test/test.js
@@ -2,3 +2,7 @@ const chair = require('chai');
 const expect = chai.expect;
 const chaiAsPromised = require('chai-as-promised');
 chai.use(chaiAsPromised).should();
+
+const Car = require('../models/car');
+
+describe('Car Model', () => {});
